eebf22b21cf19c9202d778c20daf62016bb5bcf4
alexism9 <alexism9@cs.washington.edu>
2020-02-04 14:19:04 -0800

    my branch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-
+d3.csv("SpotifyDaily.csv").then(function(data) {
+    data.forEach(function(d) {
+      d.population = +d.population;
+      d["land area"] = +d["land area"];
+    });
+    console.log(data[0]);
+  });
 
 console.log('hello world');
