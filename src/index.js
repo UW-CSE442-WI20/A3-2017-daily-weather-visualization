4c709d45068030bee497a98782b31e32feca112f
alexism9 <alexism9@cs.washington.edu>
2020-02-04 16:25:43 -0800

    added to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-d3.csv("SpotifyDaily.csv").then(function(data) {
-    data.forEach(function(d) {
-      d.population = +d.population;
-      d["land area"] = +d["land area"];
-    });
-    console.log(data[0]);
-  });
+d3.csv("../SpotifyDaily.csv", function(data) {
+  for (var i = 0; i < 10; i++) {
+      console.log(data[i]);
+  }
+});
 
-console.log('hello world');
+d3.csv("../SpotifyDaily.csv", function(data) {
+
+  for (var i = 0; i < data.length; i++) {
+    if (data[i].Country == "United States") {
+      console.log(data[i]);
+    }
+      
+  }
+});
