9f755fc76c43fc84e610fedf19c5336f746c7bf8
alexism9 <alexism9@cs.washington.edu>
2020-02-08 15:56:28 -0800

    deleted

--- src/index.js
+++ src/index.js
@@ -194,44 +194,6 @@ for (var i = 0; i < dataset.length; i++) {
   }
 }
 
-var w = 500;
-var h = 100;
-var barPadding = 1;
-
-var xScale = d3.scaleBand()
-							.domain(d3.range(dataset.length))
-							.rangeRound([0, w])
-							.paddingInner(0.05);
-			
-			var yScale = d3.scaleLinear()
-							.domain([0, d3.max(dataset, function(d) { return d.value; })])
-              .range([0, h]);
-              
-
-//Create SVG element
-var svg = d3.select("body")
-.append("svg")
-.attr("width", w)
-.attr("height", h);
-
-svg.selectAll("rect")
-.data(dataset)
-.enter()
-.append("rect")
-.attr("x", function(d, i) {
- return i * (w / dataset.length);
-})
-.attr("y", function(d) {
- return h - (d * 4);
-})
-.attr("width", w / dataset.length - barPadding)
-.attr("height", function(d) {
- return d * 4;
-})
-.attr("fill", function(d) {
-return "rgb(0, 0, " + Math.round(d * 10) + ")";
-});
-
 
 //On click, update with new data			
 d3.selectAll("p")
