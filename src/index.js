3d48f0ca6efe38ad2c04a8bd022eb02219067a60
alexism9 <alexism9@cs.washington.edu>
2020-02-08 14:26:02 -0800

    added dropdown

--- src/index.js
+++ src/index.js
@@ -18,8 +18,17 @@ var countriesName = [	'Canada',	'Denmark',	'Greece',	'Iceland',	'Mexico',	'Phili
 'Bolivia',	'Czech Republic',	'France',	'Ireland',	'Panama',	'Singapore',	'Vietnam',
 'Brazil',	'Germany',	'United Kingdom',	'Israel',	'Peru',	'Slovakia',	'global'];
 
+
+
 var dataset;
 var headerNames;
+
+
+var fileName;
+
+var countryID; 
+
+
 d3.csv("../top10perCountry2019/streamsglobal10.csv", function(error, data) {
   if (error && error.target.status === 404) {
     console.log("File not found")
@@ -39,26 +48,116 @@ d3.csv("../top10perCountry2019/streamsglobal10.csv", function(error, data) {
 setTimeout(function(){
   //console.log(dataset);
   },200);
+  
+/*
+
+d3.csv("../top10perCountry2019/streamsglobal10.csv", function(error, data) {
+  if (error && error.target.status === 404) {
+    console.log("File not found")
+  }
+  if(data.length === 0){
+  console.log("File empty")
+  }
+  var i;
+  headerNames = d3.keys(data[0]);
+
+  for (i = 0; i < data.length; i++) {
+     // console.log(data[i]);
+
+  }
+  dataset = data;
+});
+setTimeout(function(){
+  //console.log(dataset);
+  },200);
+  */
 
 //console.log(dataset.length);
 
+//On click, update with new data			
+d3.selectAll("p")
+  .on("click", function() {
 
-/*
-var filtered;
-function filter_data(selection) {
-  d3.csv("../top10perCountry2019/streamsus10.csv", function(error, data) {
+    //See which p was clicked
+    var paragraphID = d3.select(this).attr("id");
+    
+    //Decide what to do next
+    //if (paragraphID == "us") {
+      fileName = "../top10perCountry2019/streams" + paragraphID + "10.csv";
 
-      var filtered_data = data.filter(function(d) {
-          return d[6] == selection;
-      })
-      filtered = filter_data;
+      d3.csv(fileName, function(error, data) {
+        if (error && error.target.status === 404) {
+          console.log("File not found")
+        }
+        if(data.length === 0){
+        console.log("File empty")
+        }
+        var i;
+        headerNames = d3.keys(data[0]);
+      
+        for (i = 0; i < data.length; i++) {
+           console.log(data[i]);
+      
+        }
+        dataset = data;
+      });
+      setTimeout(function(){
+        //console.log(dataset);
+        },200);
+
+
   });
-  setTimeout(function(){
-    console.log(filtered);
-    },200);
-}
-*/
 
+  
+  
+  var Countries = (countriesName).slice(0);
+  Countries = Countries.sort();
+
+  var dropdown = d3.select("#vis-container")
+      .insert("select", "svg")
+      .on("change", function(){
+
+        var country = d3.select(this).property('value');
+        var index = countriesName.indexOf(country);
+      
+        
+          fileName = "../top10perCountry2019/streams" + countriesList[index] + "10.csv";
+      
+          d3.csv(fileName, function(error, data) {
+            if (error && error.target.status === 404) {
+              console.log("File not found")
+            }
+            if(data.length === 0){
+            console.log("File empty")
+            }
+            var i;
+            headerNames = d3.keys(data[0]);
+          
+            for (i = 0; i < data.length; i++) {
+               console.log(data[i]);
+          
+            }
+            dataset = data;
+          });
+          setTimeout(function(){
+            //console.log(dataset);
+            },200);
+
+
+      });
+
+
+dropdown.selectAll("option")
+.data(Countries)
+.enter().append("option")
+.attr("value", function (d) { return d; })
+.text(function (d) {
+    return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+
+    
+});
+ 
+/*
 
 //On click, update with new data			
 d3.selectAll("p")
@@ -70,99 +169,24 @@ d3.selectAll("p")
     //Decide what to do next
     if (paragraphID == "us") {
       //filtered_data(paragraphID);
-    
+      var filtered = dataset.filter(function(d) {
+        for (var i = 0; i < dataset.length; i++) {
+        return d['Position'] === '1';
+        }
+      })
+      
      //console.log(filtered.length);
      for (var i = 0; i < dataset.length; i++) {
-     // data.filter(function(d){ return d.da == "toto" })
+     
      console.log(dataset[i]);
      }
      console.log(headerNames);
+     console.log(filtered);
       
     } else {
       console.log("global")
     }
   });
-
+*/
 
  
-  /*
-
-//Width and height
-var w = 500;
-var h = 100;
-var barPadding = 1;
-
-
-//Create SVG element
-var svg = d3.select("body")
-      .append("svg")
-      .attr("width", w)
-      .attr("height", h);
-
-svg.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", function(d, i) {
-      return i * (w / data.length);
-    })
-    .attr("y", function(d) {
-      return h - (d * 4);
-    })
-    .attr("width", w / data.length - barPadding)
-    .attr("height", function(d) {
-      return d * 4;
-    })
-    .attr("fill", function(d) {
-    return "rgb(0, 0, " + Math.round(d * 10) + ")";
-    });
-
-svg.selectAll("text")
-    .data(data)
-    .enter()
-    .append("text")
-    .text(function(d) {
-      return d;
-    })
-    .attr("text-anchor", "middle")
-    .attr("x", function(d, i) {
-      return i * (w / data.length) + (w / data.length - barPadding) / 2;
-    })
-    .attr("y", function(d) {
-      return h - (d * 4) + 14;
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "11px")
-    .attr("fill", "white");
-
-
-
-*/
-
-
-//var fileName = "../top10perCountry2019/streams" + countriesList[i] + "10.csv";
-/*
-var fileName = "./top10perCountry2019/streamsus10.csv";
-var i;
-d3.csv(fileName, function(data) {
-  for (i = 0; i < 10; i++) {
-    if (data[i].Country == "United States") {
-      console.log(data[i]);
-    }
-      
-  }
-});
-
-
-
-var fileName = "../streamsus10.csv";
-var i;
-d3.csv(fileName, function(data) {
-  for (i = 0; i < 10; i++) {
-    if (data[i].Country == "United States") {
-      console.log(data[i]);
-    }
-      
-  }
-});
-*/
