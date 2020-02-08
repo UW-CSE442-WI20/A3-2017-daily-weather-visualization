0998e6e3fa0548165218cb6c79d2ae5827d76643
alexism9 <alexism9@cs.washington.edu>
2020-02-08 15:55:57 -0800

    two dropdowns but can't filter date

--- src/index.js
+++ src/index.js
@@ -19,10 +19,14 @@ var countriesName = [	'Canada',	'Denmark',	'Greece',	'Iceland',	'Mexico',	'Phili
 'Brazil',	'Germany',	'United Kingdom',	'Israel',	'Peru',	'Slovakia',	'global'];
 
 
+var dates = [	'1/1/19', '12/31/19'];
 
-var dataset;
+var dateSelect;
+var dataset = [];
 var headerNames;
+var correctDate = [];
 
+var index;
 
 var fileName;
 
@@ -40,7 +44,7 @@ d3.csv("../top10perCountry2019/streamsglobal10.csv", function(error, data) {
   headerNames = d3.keys(data[0]);
 
   for (i = 0; i < data.length; i++) {
-     // console.log(data[i]);
+      console.log(data[i]);
 
   }
   dataset = data;
@@ -50,29 +54,6 @@ setTimeout(function(){
   },200);
   
 /*
-
-d3.csv("../top10perCountry2019/streamsglobal10.csv", function(error, data) {
-  if (error && error.target.status === 404) {
-    console.log("File not found")
-  }
-  if(data.length === 0){
-  console.log("File empty")
-  }
-  var i;
-  headerNames = d3.keys(data[0]);
-
-  for (i = 0; i < data.length; i++) {
-     // console.log(data[i]);
-
-  }
-  dataset = data;
-});
-setTimeout(function(){
-  //console.log(dataset);
-  },200);
-  */
-
-//console.log(dataset.length);
 
 //On click, update with new data			
 d3.selectAll("p")
@@ -108,18 +89,20 @@ d3.selectAll("p")
 
   });
 
-  
+  */
   
   var Countries = (countriesName).slice(0);
   Countries = Countries.sort();
+  
 
   var dropdown = d3.select("#vis-container")
       .insert("select", "svg")
       .on("change", function(){
 
+       // var dateS = d3.select("#vis-container-date").property('value');
         var country = d3.select(this).property('value');
-        var index = countriesName.indexOf(country);
-      
+        index = countriesName.indexOf(country);
+      console.log(dateSelect);
         
           fileName = "../top10perCountry2019/streams" + countriesList[index] + "10.csv";
       
@@ -133,22 +116,65 @@ d3.selectAll("p")
             var i;
             headerNames = d3.keys(data[0]);
           
-            for (i = 0; i < data.length; i++) {
-               console.log(data[i]);
-          
-            }
+           
             dataset = data;
+/*
+            var filtered = dataset.filter(function(d) {
+              for (var i = 0; i < dataset.length; i++) {
+                
+              return d['date'] === '12/31/19';
+              }
+            })
+*/
+          //  console.log(filtered);
+           
           });
           setTimeout(function(){
             //console.log(dataset);
             },200);
-
+            console.log(typeof(dateSelect));
+            for (var i = 0; i < dataset.length; i++) {
+              if (dataset[i].date === dateSelect.toString()) {
+                  console.log(dataset[i]);
+              }
+            }
+            console.log(headerNames);
 
       });
 
 
+      
+
+      var dateDropdown = d3.select("#vis-container-date")
+      .insert("select", "svg")
+      .on("change", function(){
+        dateSelect = d3.select(this).property('value');
+        console.log(dateSelect);
+
+      });
+
+      
+   
+
+
+
 dropdown.selectAll("option")
 .data(Countries)
+.enter().append("option")
+.attr("value", function (d) { return d; })
+.attr("selected", function(d){
+  return d === "Global";
+})
+.text(function (d) {
+    return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
+
+    
+});
+
+
+
+dateDropdown.selectAll("option")
+.data(dates)
 .enter().append("option")
 .attr("value", function (d) { return d; })
 .text(function (d) {
@@ -157,7 +183,55 @@ dropdown.selectAll("option")
     
 });
  
-/*
+
+
+
+
+
+for (var i = 0; i < dataset.length; i++) {
+  if (dataset[i].date == dateSelect) {
+      console.log(dataset[i]);
+  }
+}
+
+var w = 500;
+var h = 100;
+var barPadding = 1;
+
+var xScale = d3.scaleBand()
+							.domain(d3.range(dataset.length))
+							.rangeRound([0, w])
+							.paddingInner(0.05);
+			
+			var yScale = d3.scaleLinear()
+							.domain([0, d3.max(dataset, function(d) { return d.value; })])
+              .range([0, h]);
+              
+
+//Create SVG element
+var svg = d3.select("body")
+.append("svg")
+.attr("width", w)
+.attr("height", h);
+
+svg.selectAll("rect")
+.data(dataset)
+.enter()
+.append("rect")
+.attr("x", function(d, i) {
+ return i * (w / dataset.length);
+})
+.attr("y", function(d) {
+ return h - (d * 4);
+})
+.attr("width", w / dataset.length - barPadding)
+.attr("height", function(d) {
+ return d * 4;
+})
+.attr("fill", function(d) {
+return "rgb(0, 0, " + Math.round(d * 10) + ")";
+});
+
 
 //On click, update with new data			
 d3.selectAll("p")
@@ -171,15 +245,15 @@ d3.selectAll("p")
       //filtered_data(paragraphID);
       var filtered = dataset.filter(function(d) {
         for (var i = 0; i < dataset.length; i++) {
-        return d['Position'] === '1';
+        return d['date'] === dateSelect;
         }
       })
       
      //console.log(filtered.length);
-     for (var i = 0; i < dataset.length; i++) {
+     //for (var i = 0; i < dataset.length; i++) {
      
-     console.log(dataset[i]);
-     }
+    // console.log(dataset[i]);
+     //}
      console.log(headerNames);
      console.log(filtered);
       
@@ -187,6 +261,5 @@ d3.selectAll("p")
       console.log("global")
     }
   });
-*/
 
  
