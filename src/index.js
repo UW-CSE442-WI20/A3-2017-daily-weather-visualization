b723930c43302e03c9d9a6e98231614e39314f9f
airraptor17 <46275492+airraptor17@users.noreply.github.com>
2020-02-11 16:12:31 -0800

    Slider Changes

--- src/index.js
+++ src/index.js
@@ -397,31 +397,110 @@ y.domain(d3.range(1, barDataset.length + 1));
 
 // add the y Axis
 svg.append("g")
-  .call(d3.axisLeft(y).tickFormat(""));
+    .call(d3.axisLeft(y).tickFormat(""));
 
 
+svg.selectAll("text.value")
+    .data(barDataset)
+    .enter()
+    .append("text")
+    .text(function (d) {
+        return songNames[parseInt(d[1]) - 1];
+    })
+    .attr("text-anchor", "end")
+    .attr("y", function (d, i) {
+        return  (9 - i) * (h / barDataset.length) + 27;
+    })
+    .attr("x", function (d) {
+        var index = d[1];
+        var streams = barDataset[index - 1][0];
+        return x(streams) - 8;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "14px")
+    .attr("font-weight", 550)
+    .attr("fill", "black");
 
-  svg.selectAll("text.value")
-  .data(barDataset)
-  .enter()
-  .append("text")
-  .text(function(d) {
-      return songNames[parseInt(d[1]) - 1];
-      
-      //d[1];
-  })
-  .attr("text-anchor", "end")
-  .attr("y", function(d, i) {
-      return (9 - i) * (h / barDataset.length ) + 27;
-  })
-  .attr("x", function(d) {
-      var index = d[1];
-      var streams = barDataset[index - 1][0];
-      return x(streams) - 8 ;
-  })
-  .attr("font-family", "sans-serif")
-  .attr("font-size", "14px")
-  .attr("font-weight", 550)
-  .attr("fill", "black");
+
+// SLIDER STUFF !!
+"use strict";
+(function () {
+
+    // MODULE GLOBAL VARIABLES AND HELPER FUNCTIONS CAN BE PLACED
+    // HERE
+    var weeks2019;
+    var sliderTime;
+    var gTime;
+
+    window.onload = function () {
+        initSlider();
+    };
+
+    function playSlider() {
+
+    }
+
+    // Code inspired/provided by https://github.com/johnwalley/d3-simple-slider v1.5.4 Copyright 2019 John Walley
+    function initSlider() {
+        weeks2019 = d3.range(0, 53).map(function (d) {
+            return new Date(2019, 0, 1 + 7 * d);
+        });
+
+        sliderTime = d3
+            .sliderBottom()
+            .min(d3.min(weeks2019))
+            .max(d3.max(weeks2019))
+            .step(28)
+            .width(1020 - margin.left - margin.right)
+            .tickFormat(d3.timeFormat('%m-%d'))
+            .tickValues(weeks2019)
+            .displayValue(false)
+            .on('onchange', val => {
+                d3.select('p#value').text(d3.timeFormat('%Y-%m-%d')(val));
+                window[sliderDate] = d3.timeFormat('%Y-%m-%d')(val);
+
+                dateSelect = d3.timeFormat('%Y-%m-%d')(val);
+                console.log(localStorage.getItem('currDate'));
+
+                var filtered = dataset.filter(function (d) {
+                    for (var i = 0; i < dataset.length; i++) {
+                        return d['date'] === dateSelect;
+                    }
+                })
+
+                var barDataset = [[]];
+                var songNames = [''];
+                for (var i = 0; i < filtered.length; i++) {
+                    var arrayObj = [parseInt(filtered[i].Streams), (10 - i) + ""];
+                    var name = filtered[i]['Track Name'];
+                    if (filtered[i]['Track Name'].length > 30) {
+                        name = name.substring(0, 31) + "...";
+                    }
+                    songNames[i] = name;
+                    barDataset[i] = arrayObj;
+                }
+
+                updateGraph(barDataset, songNames)
+            });
+
+        gTime = d3
+            .select('div#slider')
+            .append('svg') 
+            .attr('width', 1100 - margin.left - margin.right)
+            .attr('height', 132 - margin.top - margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(30,30)');
+
+        gTime.call(sliderTime);
+
+        gTime.selectAll("text").attr("dx", "-10px").attr("dy", "-16px");
+
+        //initializes date shown on screen
+        d3.select('p#value').text(d3.timeFormat('%Y-%m-%d')(sliderTime.value()));
+        sliderDate = d3.timeFormat('%Y-%m-%d')(sliderTime.value());
+
+    }
+})();
+
 
 
